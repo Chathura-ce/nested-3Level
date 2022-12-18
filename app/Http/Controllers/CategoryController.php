b94518c6f686f98ab1fa490857244ce35fd2131d
Chathura-ce <chathu.eac@gmail.com>
2022-12-18 12:17:53 +0530

    Initial commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::whereNull('category_id')
+            ->with(['subcategories.subcategories'=>function($query){
+                $query->withCount('products');
+            }])->get();
+
+        return view('categories.index', compact('categories'));
+    }
+
+    public function show(Category $category)
+    {
+        $category->load('subcategories.subcategories');
+
+        $subcategoryIDs = [$category->id];
+        foreach ($category->subcategories as $subcategory) {
+            $subcategoryIDs[] = $subcategory->id;
+            foreach ($subcategory->subcategories as $subsubcategory) {
+                $subcategoryIDs[] = $subsubcategory->id;
+            }
+        }
+/*echo "<pre>";
+        print_r($subcategoryIDs);
+echo "</pre>";die;*/
+
+        $products = Product::whereHas('categories',function ($query)use($subcategoryIDs){
+            $query->whereIn('categories.id',$subcategoryIDs);
+        })->get();
+
+        return view('categories.show', compact('category', 'products'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [\App\Http\Controllers\CategoryController::class, 'index']);
+Route::get('/category/{category}', [\App\Http\Controllers\CategoryController::class, 'show'])
+    ->name('categories.show');
